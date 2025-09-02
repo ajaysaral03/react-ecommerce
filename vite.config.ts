13a6fe08336fbc994401ef61150333a5d69dcd3b
Ajay Saral <ajaysaral03@gmail.com>
2025-09-02 13:31:33 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/react-ecommerce/",  // 👈 यहाँ repo का नाम डालें
-  plugins: [react()],
+  base: "/react-ecommerce/",  // 👈 Make sure this exactly matches your GitHub repo name
+  plugins: [react()]
 })
